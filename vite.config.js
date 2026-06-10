9e0d8b19d46d211e21dbff3d93c09cc560dfcc41
volpreim <jhoanps13@gmail.com>
2026-06-10 01:40:11 -0500

    Agregar vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Bordados-GyC/', // <-- Esta línea es la clave para que encuentre tus archivos
-  plugins: [react()]
+  base: '/Bordados-GyC/',
+  plugins: [react()],
 })
