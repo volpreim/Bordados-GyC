f9707c8b18a511cb45fd8513bcddbfed61e9ee96
volpreim <jhoanps13@gmail.com>
2026-06-10 01:59:51 -0500

    Agregar solucion

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/Bordados-GyC/',
   plugins: [react()],
 })
